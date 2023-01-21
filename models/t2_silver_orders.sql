5d6429322eda611fce828d849300f28e3823cd7f
mendelsohn.chan@databricks.com <mendelsohn.chan@databricks.com>
2023-01-21 04:20:54 +0000

    Commit 2023-01-21

--- models/t2_silver_orders.sql
+++ models/t2_silver_orders.sql
@@ -5,7 +5,7 @@
 -- order data cleaned and anonymized for analysis -- 
 select
   cast(amount as int),
-  id as order_id,
+  `id` as order_id,
   user_id,
   cast(item_count as int),
   to_timestamp(transaction_date, "MM-dd-yyyy HH:mm:ss") as creation_date
